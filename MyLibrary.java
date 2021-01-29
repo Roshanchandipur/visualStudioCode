7d4c34ebcef4829ea95f4c8ca0e965ec5747b6b0
ROSHAN KUMAR <roshanchandipur@gmail.com>
2021-01-29 16:47:44 +0530

    PathFinder added

---
+++ MyLibrary.java
@@ -0,0 +1,11 @@
+public class MyLibrary{
+
+    public static void main(String [] args){
+
+        LinkedListL ll = new LinkedListL();
+        for(int i=0;i<10;i++) ll.add(i);
+        ll.print();
+
+    }
+
+}
